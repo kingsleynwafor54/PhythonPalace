3ef98f6d9008f16b9c966c47733346e68aadfcc6
Kingsley Nwafor <kingsleynwafor54@gmail.com>
2021-09-24 07:24:36 +0100

    OOP_IN_Python

---
+++ pythonProject4/Composition.py
@@ -0,0 +1,24 @@
+class Animal:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+    def getAge(self):
+        return self.age
+
+    def getName(self):
+       return self.name
+
+    def setName(self, name):
+        self.name=name;
+
+    def setAge(self,age):
+        self.age=age
+
+
+
+d= Animal("django",12)
+print(d.getAge())
+
+
