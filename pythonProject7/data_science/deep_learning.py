f04c3c9241438d152a7daae763938e5b6e5578ad
Kingsley Nwafor <kingsleynwafor54@gmail.com>
2022-01-11 10:01:33 +0100

    deep learning

---
+++ pythonProject7/data_science/deep_learning.py
@@ -0,0 +1,14 @@
+# Importing Libraries
+
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import  MinMaxScaler
+import  matplotlib.pyplot as plt
+
+
+#import talib
+#import random
+
+#Import data
+data=pd.read_csv("data.csv")
